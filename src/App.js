7ea67da6cd93b57c9c15ecac8d76f8949715da9e
TOGGO <shinhc5@gmail.com>
2020-11-12 15:40:12 +0900

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button>뚀</button>
+        <button>김도희</button>
         <a
           className="App-link"
           href="https://reactjs.org"
